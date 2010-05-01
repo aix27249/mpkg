a72dc9837fdf6f06e3aef321a77ea07f648fc097
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2010-05-01 18:12:13 +0000

    added openbox live build scripts
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@40 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

---
+++ installer/initrd.openbox/make_rootfs.sh
@@ -0,0 +1,14 @@
+mksquashfs /mnt/hd/initrd.openbox rootfs.sfs -e bin lib lib64 sbin usr
+
+ARCH=`uname -m`
+
+cd initrd.new
+mksquashfs bin bin.sfs
+mksquashfs sbin sbin.sfs
+mksquashfs lib lib.sfs
+if [ "$ARCH" = "x86_64" ] ; then
+	mksquashfs lib64 lib64.sfs
+fi
+mksquashfs usr usr.sfs -e local
+mksquashfs usr/local local.sfs
+cd -
