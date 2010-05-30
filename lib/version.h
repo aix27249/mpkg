ef3b4e6309fb1573566e1af2017f4481af1221ff
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2010-05-30 07:49:33 +0000

    version bump to 2010.05.30 snapshot 4
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@99 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

--- lib/version.h
+++ lib/version.h
@@ -1,7 +1,7 @@
 #ifndef MPKG_VERSION_H_
 #define MPKG_VERSION_H_
 #include "build_counter.h"
-#define DISTRO_VERSION "2010.05.21 (snapshot 3)"
+#define DISTRO_VERSION "2010.05.30 (snapshot 4)"
 #define mpkgVersion "0.14.1 (devel)"
 #define mpkgBuild MPKG_BUILD
 
