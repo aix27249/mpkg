06e0a88b2540a2bec8e5681b8e887a03cd31dfa2
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2010-06-26 21:51:00 +0000

    dhcpconf: write mode
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@118 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

--- installer/initrd.openbox/make_local.sh
+++ installer/initrd.openbox/make_local.sh
@@ -1,4 +1,6 @@
+rm rootfs.sfs
 mksquashfs /mnt/hd/initrd.openbox rootfs.sfs -e bin lib lib64 sbin usr
 cd initrd.new
+rm local.sfs
 mksquashfs usr/local local.sfs
 cd -
--- installer/initrd.openbox/make_rootfs.sh
+++ installer/initrd.openbox/make_rootfs.sh
@@ -1,15 +1,14 @@
+rm rootfs.sfs
 mksquashfs /mnt/hd/initrd.openbox rootfs.sfs -e bin lib lib64 sbin usr
-if [ "$1" == "root" ] ; then
-	exit 0
-fi
-
 ARCH=`uname -m`
 
 cd initrd.new
+rm bin.sfs sbin.sfs lib.sfs usr.sfs local.sfs
 mksquashfs bin bin.sfs
 mksquashfs sbin sbin.sfs
 mksquashfs lib lib.sfs
 if [ "$ARCH" = "x86_64" ] ; then
+	rm lib64.sfs
 	mksquashfs lib64 lib64.sfs
 fi
 mksquashfs usr usr.sfs -e local
