77f3d5d77d8fa91c5f0e409ed35171a716769f93
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2010-10-06 20:51:56 +0000

    nvidia selector seems to be ready. Time to add it to live image.
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@175 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

--- lib/version.h
+++ lib/version.h
@@ -1,8 +1,8 @@
 #ifndef MPKG_VERSION_H_
 #define MPKG_VERSION_H_
 #include "build_counter.h"
-#define DISTRO_VERSION "2010.08 (snapshot 5)"
-#define mpkgVersion "0.15"
+#define DISTRO_VERSION "2010.10 (snapshot 5)"
+#define mpkgVersion "0.16"
 #define mpkgBuild MPKG_BUILD
 
 #endif
--- misc_tools/nvidia_driver_chooser.sh
+++ misc_tools/nvidia_driver_chooser.sh
@@ -1,9 +1,54 @@
 #!/bin/sh
+# Script that asks user what nvidia driver he wants to use.
+# It will be used in LiveDVD environment in fallback stuff
 
+# Prerequirements: 
+#   nvidia-*.txz in /var/mpkg/cache
+#   working mpkg environment
+
+# Script should be run BEFORE any X11 environments.
+
+
+# Define a card class we want to detect
 CARD_CLASS=nVidia
+
+# I use this for testing on my macbook, so do not mention it
+#CARD_CLASS=Intel
 
 
 HAS_NVIDIA=`lspci | grep $CARD_CLASS | grep 'VGA compatible controller'`
 if [ "$HAS_NVIDIA" != "" ] ; then
-	echo "We have $CARD_CLASS card: $HAS_NVIDIA"
+	dialog --menu "You have NVIDIA card: $HAS_NVIDIA. Please choose driver for it:" 25 80 70 256 "nvidia-driver-256.52" 173 "nvidia-driver-legacy-173" 96 "nvidia-driver-legacy96" "nv" "Generic nv" "nouveau" "New KMS-driven nouveau. Probably you don't want it." 2> /tmp/nvcheck
+	CHOOSEN_CARD=`cat /tmp/nvcheck`
+	rm /tmp/nvcheck
+
+	case $CHOOSEN_CARD in 
+		256)
+			echo "Blob v.256 selected"
+			rmmod nouveau
+			mpkg-install -y /var/mpkg/cache/nvidia-driver-256*.txz /var/mpkg/cache/nvidia-kernel-256*.txz
+			;;
+		173)
+			echo "Legacy blob v.173 selected"
+			rmmod nouveau
+			mpkg-install -y /var/mpkg/cache/nvidia-driver-legacy173*.txz /var/mpkg/cache/nvidia-kernel-legacy173*.txz
+			;;
+		96)
+			echo "Legacy old blob v.96 selected"
+			rmmod nouveau
+			mpkg-install -y /var/mpkg/cache/nvidia-driver-legacy96*.txz /var/mpkg/cache/nvidia-kernel-legacy96*.txz
+			;;
+		nv)
+			echo "Simple nv driver selected"
+			rmmod nouveau
+			cp /etc/X11/10-nv.conf /etc/X11/xorg.conf.d/
+			;;
+		nouveau)
+			echo "Nouveau selected"
+			;;
+		*)
+			echo "Nouveau selected"
+			;;
+	esac
 fi
+
