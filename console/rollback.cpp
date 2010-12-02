5373126b0f53833384b477da9c02077ffe21f107
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2010-12-02 12:35:26 +0000

    reworked rollback. Now it seems to be fully functional, except searching functions
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@226 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

--- console/rollback.cpp
+++ console/rollback.cpp
@@ -10,101 +10,104 @@ string decodeStatus(string status) {
 	return "UNKNOWN";
 }
 int print_usage() {
-	fprintf(stderr, _("mpkg-rollback: rolls back installed package set to specified state.\nUsage: \n\tmpkg-rollback TRANSACTION_ID\tRolls back up to specified transaction. All transactions with ID equal or greater than specified TRANSACTION_ID will be reverted.\n"));
+	fprintf(stderr, _("mpkg-rollback: rolls back installed package set to specified state.\nUsage: \n\tmpkg-rollback [OPTIONS] TRANSACTION_ID\tRolls back up to specified transaction. All transactions with ID equal or greater than specified TRANSACTION_ID will be reverted.\n\nAvailable options:\n\t-v\t--verbose\tVerbose info\n"));
 	return 1;
+}
+
+void printWarning(string type, string name, string ver, string build) {
+	mWarning("Cannot find " + name + " " + ver + "-" + build + " within available packages. Rollback may be incomplete.");
 }
 int main(int argc, char **argv) {
 	interactive_mode=true;
 
 	if (argc<2) return print_usage();
 
-	int rollback_to = atoi(argv[1]);
+	bool verbose = false;
+	int rollback_to = 0;
+	for (int i=1; i<=argc; ++i) {
+		if (string(argv[i])=="-v" || string(argv[i])=="--verbose") verbose = true;
+		else if (atoi(argv[i])>0) {
+			rollback_to = atoi(argv[i]);
+		}
+	}
 	if (rollback_to==0) return print_usage();
 
 
+	printf(_("Gathering data\n"));
 	SQLRecord sqlSearch;
 	SQLTable transactions;
 	SQLTable transaction_details;
 
 	mpkg core;
+
+	core.clean_queue();
 	core.db->get_sql_vtable(transactions, sqlSearch, "transactions", sqlSearch);
 	core.db->get_sql_vtable(transaction_details, sqlSearch, "transaction_details", sqlSearch);
 
+	PACKAGE_LIST pkgListModified;
+	core.get_packagelist(sqlSearch, &pkgListModified);
+
+	printf(_("Searching for transactions\n"));
+	
 	int fTr_id=0;
-	int fTr_date_start=1;
+	//int fTr_date_start=1;
 	int fTr_date_end=2;
-	int fTr_status=3;
+	//int fTr_status=3;
 	int fTrd_tid=1;
 	int fTrd_pkgname=2;
 	int fTrd_pkgver=3;
 	int fTrd_pkgbuild=4;
+	int fTrd_md5=5;
 	int fTrd_action=6;
-	int fTrd_date=7;
+	//int fTrd_date=7;
 
 
 
 	vector<string> ip_name, ip_version, ip_build, rp_name;
+	int pkgNum;
 
-
-	for (size_t i=transactions.size()-1; i>=0 && i<(size_t) -1; i--) {
-		printf("%sRolling back transaction ID:%s %s\nStarted: %s\nFinished at: %s\nStatus: %s\n", CL_GREEN, CL_WHITE, transactions.getValue(i, fTr_id).c_str(), transactions.getValue(i, fTr_date_start).c_str(), transactions.getValue(i, fTr_date_end).c_str(), decodeStatus(transactions.getValue(i, fTr_status)).c_str());
-
+	// Going thru transactions from the end.
+	for (size_t i=transactions.size()-1; (int) i>=0; i--) {
+		// If ID is less than required, skip it
 		if (atoi(transactions.getValue(i, fTr_id).c_str())<rollback_to) continue;
-		for (size_t t=0; t<transaction_details.size(); ++t) {
-			if (atoi(transactions.getValue(i, fTr_id).c_str())<rollback_to) continue;
-			if (transaction_details.getValue(t, fTrd_tid)!=transactions.getValue(i, fTr_id)) continue;
-			if (transaction_details.getValue(t, fTrd_action)!="0") continue; // Install only
+		// Print info
+		printf("%sRolling back transaction %s%s (%s)\n", CL_GREEN, CL_WHITE, transactions.getValue(i, fTr_id).c_str(), transactions.getValue(i, fTr_date_end).c_str());
 
-			rp_name.push_back(transaction_details.getValue(t, fTrd_pkgname));
+		// Packages that was installed
+		for (size_t t=0; t<transaction_details.size(); ++t) {
+			if (transaction_details.getValue(t, fTrd_tid)!=transactions.getValue(i, fTr_id)) continue; // Skip other transaction details
+		
+			// Now look by MD5.
+			pkgNum = pkgListModified.getPackageNumberByMD5(transaction_details.getValue(t, fTrd_md5));
+			if (pkgNum==-1) {
+				// This is not fatal, but can cause incomplete rollback in some cases. Warn user about this.
+				printWarning(transaction_details.getValue(t, fTrd_action), transaction_details.getValue(t, fTrd_pkgname), transaction_details.getValue(t, fTrd_pkgver), transaction_details.getValue(t, fTrd_pkgbuild));
+				continue;
+			}
+			if (transaction_details.getValue(t, fTrd_action)=="0") {
+				if (!pkgListModified[pkgNum].installed()) pkgListModified.get_package_ptr(pkgNum)->set_action(ST_NONE, "");
+				else pkgListModified.get_package_ptr(pkgNum)->set_action(ST_REMOVE, "rollback");
+				if (verbose) printf("--- %s %s\n", pkgListModified[pkgNum].get_name().c_str(), pkgListModified[pkgNum].get_fullversion().c_str());
+			}
+			else {
+				if (pkgListModified[pkgNum].installed()) pkgListModified.get_package_ptr(pkgNum)->set_action(ST_NONE, "");
+				else pkgListModified.get_package_ptr(pkgNum)->set_action(ST_INSTALL, "rollback");
+				if (verbose) printf("+++ %s %s\n", pkgListModified[pkgNum].get_name().c_str(), pkgListModified[pkgNum].get_fullversion().c_str());
+			}
 			
-			// Search for such package in ip_ vectors, and remove them if they are found there
-			for (size_t q=0; q<ip_name.size(); ++q) {
-				if (ip_name[q]==transaction_details.getValue(t, fTrd_pkgname)) {
-					ip_name.erase(ip_name.begin()+q);
-					ip_version.erase(ip_version.begin()+q);
-					ip_build.erase(ip_build.begin()+q);
-					break;
-				}
 
-			}
 		}
+	}	
+	// Committing queue.
 
-		for (size_t t=0; t<transaction_details.size(); ++t) {
-			if (atoi(transactions.getValue(i, fTr_id).c_str())<rollback_to) continue;
-			if (transaction_details.getValue(t, fTrd_tid)!=transactions.getValue(i, fTr_id)) continue;
-			if (transaction_details.getValue(t, fTrd_action)!="1") continue; // Remove only
-
-			// Look in remove queue. If such package is there, remove it
-			for (size_t q=0; q<rp_name.size(); ++q) {
-				if (rp_name[q]==transaction_details.getValue(t, fTrd_pkgname)) {
-					rp_name.erase(rp_name.begin()+q);
-					break;
-				}
-
-			}
-
-			// Look in install queue. If such package is already there, erase it
-			for (size_t q=0; q<ip_name.size(); ++q) {
-				if (ip_name[q]==transaction_details.getValue(t, fTrd_pkgname)) {
-					ip_name.erase(ip_name.begin()+q);
-					ip_version.erase(ip_version.begin()+q);
-					ip_build.erase(ip_build.begin()+q);
-					break;
-				}
-
-			}
-
-
-			ip_name.push_back(transaction_details.getValue(t, fTrd_pkgname));
-			ip_version.push_back(transaction_details.getValue(t, fTrd_pkgver));
-			ip_build.push_back(transaction_details.getValue(t, fTrd_pkgbuild));
+	for (size_t i=0; i<pkgListModified.size(); ++i) {
+		if (pkgListModified[i].action()==ST_REMOVE) rp_name.push_back(pkgListModified[i].get_name());
+		if (pkgListModified[i].action()==ST_INSTALL) {
+			ip_name.push_back(pkgListModified[i].get_name());
+			ip_version.push_back(pkgListModified[i].get_version());
+			ip_build.push_back(pkgListModified[i].get_build());
 		}
 	}
-	
-	// Committing queue.
-	// First, reset it
-	core.clean_queue();
-
 	// Now, add purge and install queue
 	core.purge(rp_name);
 	core.install(ip_name, &ip_version, &ip_build);
