bddd2fac49be3e149c2ce1d94cca38bb907fc187
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2010-12-22 20:58:33 +0000

    added chroot installer
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@231 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

---
+++ chroot_installer/install_virtual_machine.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Configuration variables. I think it's names and content shows everything.
+NODE=${NODE:-/tmp/new_chroot}
+REPO=${REPO:-http://packages.agilialinux.ru/}
+LIST=minimal.list
+
+echo Node will be installed in $NODE
+echo Repository: $REPO
+
+# Creating directory structure
+CWD=$(pwd)
+rm -rf $NODE
+mkdir -p ${NODE}/{etc,tmp}
+mkdir -p ${NODE}/var/mpkg/{packages,scripts,configs,backup}
+
+# Copying 
+cp $CWD/mpkg.xml.system ${NODE}/etc/mpkg.xml.system
+cat $CWD/mpkg.xml | sed -e s#NODE#$NODE#g | sed -e s#REPOSITORY#$REPO#g> ${NODE}/etc/mpkg.xml
+cp packages.db ${NODE}/var/mpkg/
+
+# Installing
+mpkg-update --conf=${NODE}/etc/mpkg.xml --sysroot=${NODE}
+mpkg-installfromlist --conf=${NODE}/etc/mpkg.xml --sysroot=${NODE} -y $CWD/${LIST_NAME}
+
+# Moving config
+mv ${NODE}/etc/mpkg.xml.system ${NODE}/etc/mpkg.xml
+echo "Installation complete"
