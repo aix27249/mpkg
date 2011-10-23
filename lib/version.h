1612703bd294f6e738b46cc36933ddee18682f0f
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2011-10-23 03:18:37 +0000

    version bump
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@557 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

--- lib/version.h
+++ lib/version.h
@@ -1,7 +1,7 @@
 #ifndef MPKG_VERSION_H_
 #define MPKG_VERSION_H_
 #include "build_counter.h"
-#define DISTRO_VERSION "8.0.0"
+#define DISTRO_VERSION "8.0.1"
 #define mpkgVersion "0.16"
 #define mpkgBuild MPKG_BUILD
 
