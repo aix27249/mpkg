135e22a9f6b8f17944556e10170da1524fd7dfe5
aix27249 <aix27249@32a6d772-3a6b-4160-9f27-08b1a0ab14b6>
2012-01-30 15:43:17 +0000

    mpkg-checkdeps: support for checking multiple repos at once
    git-svn-id: svn://agilialinux.ru/mpkg/mpkg/trunk@576 32a6d772-3a6b-4160-9f27-08b1a0ab14b6

--- console/checkdeps.cpp
+++ console/checkdeps.cpp
@@ -1,21 +1,39 @@
 #include <mpkg/libmpkg.h>
 #include <mpkg/dbstruct.h>
+
+int print_usage(int ret = 0) {
+	FILE *fd = stdout;
+	if (ret!=0) fd = stderr;
+	fprintf(fd, _("mpkg-checkdeps: checks dependency tree integrity within specified repositories. Used to find out unresolvable dependencies\n"));
+	fprintf(fd, _("USAGE: mpkg-checkdeps REPOSITORY_URL [REPOSITORY_URL] [...]\n"));
+	fprintf(fd, _("\nNote: Due to internal limitations, currently mpkg-checkdeps can check only repositores that match system architecture. For example, you cannot check x86_64 repository from i686 system\n"));
+	return ret;
+}
+
 int main(int argc, char **argv) {
 	string path;
-	if (argc>1) path=argv[1];
-	//WriteFile("sqlite_dump.sql", getDBStructure());
-	if (path.find("://")==std::string::npos) {
-		path = "./";
-		if (!FileExists(path + "packages.xml.xz") && !FileExists(path+"packages.xml.gz")) {
-			mError(_("Index file ") + path+_("packages.xml.xz or packages.xml.gz not found"));
-			return 1;
-		}
-		path = "file://" + getAbsolutePath(path) + "/";
-	}
 	PACKAGE_LIST checkList;
+	PACKAGE_LIST *tmpList;
 	Repository rep;
-	rep.get_index(path, &checkList);
-	say(_("Checking %d packages in %s\n"), checkList.size(), path.c_str());
+	if (argc==1) return print_usage(-1);
+
+	for (int i=1; i<argc; ++i) {
+		path=argv[i];
+		//WriteFile("sqlite_dump.sql", getDBStructure());
+		if (path.find("://")==std::string::npos) {
+			path = "./";
+			if (!FileExists(path + "packages.xml.xz") && !FileExists(path+"packages.xml.gz")) {
+				mError(_("Index file ") + path+_("packages.xml.xz or packages.xml.gz not found"));
+				return 1;
+			}
+			path = "file://" + getAbsolutePath(path) + "/";
+		}
+		tmpList = new PACKAGE_LIST;
+		rep.get_index(path, tmpList);
+		checkList.add(*tmpList);
+		delete tmpList;
+	}
+	say(_("Checking %d packages\n"), checkList.size());
 	for (size_t i=0; i<checkList.size(); ++i) {
 		checkList.get_package_ptr(i)->set_action(ST_INSTALL, "check");
 	}
