691eb7c97dfcc1791ccd6ab58ec83cffb0ae681b
aix27249 <i27249@gmail.com>
2012-09-22 04:51:24 +0400

    mpkg version bump

--- lib/version.h
+++ lib/version.h
@@ -1,8 +1,8 @@
 #ifndef MPKG_VERSION_H_
 #define MPKG_VERSION_H_
 #include "build_counter.h"
-#define DISTRO_VERSION "8.0.1"
-#define mpkgVersion "0.16"
+#define DISTRO_VERSION "8.1"
+#define mpkgVersion "0.17"
 #define mpkgBuild MPKG_BUILD
 
 #endif
